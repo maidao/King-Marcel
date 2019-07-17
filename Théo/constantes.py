c6ef8e4f1af174c5d1b50d423cb1fac3d99ce586
theoflaus <flaus.theo69@gmail.com>
2019-07-17 13:55:32 +0200

    Ajout fichier constantes

---
+++ Théo/constantes.py
@@ -0,0 +1,31 @@
+valeurs_jours = {
+    '1 - Lundi': 1,
+    '2 - Mardi': 2,
+    '3 - Mercredi': 3,
+    '4 - Jeudi': 4,
+    '5 - Vendredi': 5,
+    '6 - Samedi': 6,
+    '7 - Dimanche': 7
+}
+
+listes_jour = {
+    1 : 'Lundi',
+    2 : 'Mardi',
+    3 : 'Mercredi',
+    4 : 'Jeudi',
+    5 : 'Vendredi',
+    6 : 'Samedi',
+    7 : 'Dimanche'
+}
+
+days_in_week = 7
+
+jours = [
+    'Lundi',
+    'Mardi',
+    'Mercredi',
+    'Jeudi',
+    'Vendredi',
+    'Samedi',
+    'Dimanche'
+]
